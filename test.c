437e6df8e8a96535aeb54cea9ab17f5794d337b1
UH5555 <zhuhao162@163.com>
2022-05-04 13:00:00 +0800

    0504\动态内存分配（五）

--- test.c
+++ test.c
@@ -284,22 +284,105 @@
 //	//未释放动态内存开辟的空间，会出现内存泄露。
 //}
 
-void Test(void)
+//void Test(void)
+//{
+//	char* str = (char*)malloc(100);
+//	strcpy(str, "hello");
+//	free(str);//free释放str指向的空间后，并不会把str置为NULL;
+//	//str = NULL;
+//	if (str != NULL)
+//	{
+//		strcpy(str, "world");
+//		printf(str);
+//	}
+//	//非法访问内存
+//}
+//
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+////struct S
+////{
+////	int n;
+////	int arr[];//未知大小
+////};
+//
+//struct S
+//{
+//	int n;
+//	int arr[0];//未知大小 - 柔性数组成员 - 数组的大小是可以调整的
+//};
+//
+//int main()
+//{
+//	struct S s;
+//	printf("%d\n", sizeof(s));//4
+//	struct S* ps = (struct S*)malloc(sizeof(struct S) + 5 * sizeof(int));
+//	ps->n = 100;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	struct S* ptr = realloc(ps, 44);
+//	if (ptr != NULL)
+//	{
+//		ps = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//	//释放
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+
+struct S
 {
-	char* str = (char*)malloc(100);
-	strcpy(str, "hello");
-	free(str);//free释放str指向的空间后，并不会把str置为NULL;
-	//str = NULL;
-	if (str != NULL)
-	{
-		strcpy(str, "world");
-		printf(str);
-	}
-	//非法访问内存
-}
+	int n;
+	int* arr;
+};
 
 int main()
 {
-	Test();
+	struct S* ps = (struct S*)malloc(sizeof(struct S));
+	ps->arr = malloc(5*sizeof(int));
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		ps->arr[i] = i;
+	}
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", ps->arr[i]);
+	}
+	//调整大小
+	int* ptr = realloc(ps->arr, 10 * sizeof(int));
+	if (ptr != NULL)
+	{
+		ps->arr = ptr;
+	}
+	for (i = 5; i < 10; i++)
+	{
+		ps->arr[i] = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", ps->arr[i]);
+	}
+	//释放内存
+	free(ps->arr);
+	ps->arr = NULL;
+	free(ps);
+	ps = NULL;
 	return 0;
 }
